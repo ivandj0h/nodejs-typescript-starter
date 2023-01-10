0a301ac927dc35ba53dc49c3366dbdb6db42d260
ivandjoh <ivandi.djoh@gmail.com>
2023-01-10 21:56:03 +0800

    add dotenv

---
+++ module.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        APP_SERVER: string;
+        APP_PORT: string;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-const userName: string = 'John Doe';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-console.log(userName);
+const userName: string = 'John Doe';
+const port: string = process.env.APP_PORT;
+const server: string = process.env.APP_SERVER;
+
+console.log(`This ${userName} running on ${server}:${port}`);
